7900d829cf361ff89c85c5d34f902be9fb564bbd
RUGVEDLANGHI17 <rugvedlanghi18@gmail.com>
2023-06-30 21:24:14 +0530

    homepage

---
+++ src/Components/index.js
@@ -0,0 +1,9 @@
+import card from "./card";
+import FormField from "./FormField";
+import Loader from "./Loader";
+
+export{
+    card,
+    FormField,
+    Loader
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { Loader, card, Fromfield} from '../Components';
 
 const Home = () => {
+  const [loading, setLoading] = useState(false);
+  const [allPosts, setAllPosts] = useState(null);
   return (
-    <div>Home</div>
+    <section className='max-w-7xl mx-auto'>
+      <div>
+        <h1 className='font-extrabold text-[#222328] text-[32px]'>The community Showcase </h1>
+        <p className='mt-2 text-[#666e75] text-[18px] max-w[500px]'>Browse the images that you never imagined by using DALL-E open AI</p>
+        </div>
+    </section>
   )
 }
 
