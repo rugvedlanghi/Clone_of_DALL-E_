9befbb8ee6563ae529c695c2ec0d396e0da63c45
RUGVEDLANGHI17 <rugvedlanghi18@gmail.com>
2023-06-30 20:36:18 +0530

    connectedpages

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,14 @@ const App = () => {
           <img src={logo} alt="logo" className="w-28 oj=bject-contain" />
         </Link>
 
-        <Link to="/create-post" className='font-inter font-medium bg-[#6469ff] text-white px-4 px-2 rounded-md'>Create</Link> 
+        <Link to="/create-post" className='font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md'>Create</Link> 
       </header>
+      <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]'>
+        <Routes>
+          <Route path="/" element= {<Home/>} />
+          <Route path="/create-post" element= {<CreatePost/>} />
+        </Routes>
+      </main>
     </BrowserRouter>
   )
 }
